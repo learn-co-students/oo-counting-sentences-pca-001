314827cc6fc09028eeda706c5eeb4312e6dfd3c1
Pat McClernan <patmccler@gmail.com>
2020-05-04 14:57:53 -0400

    implement methods

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,17 @@
-require 'pry'
-
 class String
-
   def sentence?
-    
+    end_with? "."
   end
 
   def question?
-
+    end_with? "?"
   end
 
   def exclamation?
-
+    end_with? "!"
   end
 
   def count_sentences
-
+    split.count { |word| word.sentence? || word.question? || word.exclamation? }
   end
 end
