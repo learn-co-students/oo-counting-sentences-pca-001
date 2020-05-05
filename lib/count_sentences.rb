aae7d4c4002113745de969124df6877496a9087c
Liam Tinney <ltinney@powerhrg.com>
2020-05-05 09:34:16 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+      end 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+      end 
   end
 
   def count_sentences
-
+    fresh_array = self.split(/[.?!]/)
+    fresh_array.delete_if{|sentence| sentence.empty?}
+    fresh_array.length
   end
 end
